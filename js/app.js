81666c3527c664717e066a41a782ef47889d5d22
mlyles <mlyles@constantcontact.com>
2015-02-02 13:50:56 -0800

    new files added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+(function() {
 var app = angular.module('store',[]);
+
+// var gem = {
+// 	name: 'Item Name',
+// 	price: 2.95,
+// 	description: 'some kind of description goes here'
+// }
+
+
+app.controller('StoreController', function(){
+
+	this.product = gem;
+
+});
+
+var gem = {
+	name: 'Item Name',
+	price: 2.95,
+	description: 'some kind of description goes here',
+	canPurchase: true
+}
+
+})();
