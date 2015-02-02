17cf152cfea5c298210e2b9a49becb3d9b032c0a
mlyles <mlyles@constantcontact.com>
2015-02-02 15:29:17 -0800

    new stuff

--- js/app.js
+++ js/app.js
@@ -17,14 +17,26 @@ app.controller('StoreController', function(){
 var gems = [
 	{
 		name: 'Item Name',
-		price: 2.95,
+		price: 2,
 		description: 'some kind of description goes here',
+		images: [
+			{
+				full: 'image-01-full.jpg',
+				thumb: 'image-01-thumb.jpg'
+			}
+		],
 		canPurchase: true
 	},
 	{
 		name: 'Item Name 2',
 		price: 5.95,
 		description: 'Another some kind of description goes here',
+		images: [
+			{
+				full: 'image-01-full.jpg',
+				thumb: 'image-01-thumb.jpg'
+			}
+		],
 		canPurchase: true
 	}
 ];
